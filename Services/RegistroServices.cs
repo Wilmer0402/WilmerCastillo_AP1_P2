12fd0317b1b8580161e91547492e5915fea46955
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2024-11-14 18:06:22 -0400

    Creacion de las Plantillas del RegistroPages y del Services

---
+++ Services/RegistroServices.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using WilmerCastillo_AP1_P2.DAL;
+using WilmerCastillo_AP1_P2.Models;
+
+
+namespace WilmerCastillo_AP1_P2.Services;
+
+public class RegistroServices(IDbContextFactory<Context> DbFactory)
+{
+    private async Task<bool> Existe(int id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+    }
+
+    private async Task<bool> Insertar(Registro registro)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+    }
+
+    private async Task<bool> Modificar(Registro registro)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+    }
+
+    public async Task<bool> Guardar(Registro registro)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+    }
+
+    public async Task<bool> Eliminar(Registro registro)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+    }
+
+    public async Task<Registro> Buscar(int id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+    }
+
+    public async Task<List<Registro>> Listar(Expression<Func<Registro, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+    }
+}
